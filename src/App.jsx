c36f14730755dca494c1d0ba94dd8c897d94ad8d
ramaherdianto <ramadhanherdianto35@gmail.com>
2022-11-22 10:31:07 +0700

    feat: add feature movie detail

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,24 @@
 import { useEffect, useState } from 'react';
 import axios from 'axios';
+import { Box, styled } from '@mui/system';
+import ModalUnstyled from '@mui/base/ModalUnstyled';
+import Fade from '@mui/material/Fade';
 import { getMovieList, movieIMG, baseURL, apiKEY } from './api';
 
 function App() {
     const [popularMovies, setPopularMovies] = useState([]);
     const [movieDetail, setMovieDetail] = useState([]);
     const [keyword, setKeyword] = useState('');
-
-    // const search = async (q) => {
-    //     const query = await searchMovie(q);
-    //     setPopularMovies(query.results);
-    // };
+    const [genres, setGenres] = useState([]);
+    const [open, setOpen] = useState(false);
+    const handleClose = () => setOpen(false);
 
     const handleMovieDetail = async (id) => {
         try {
             const req = await axios.get(`${baseURL}/movie/${id}?api_key=${apiKEY}`);
-            console.log(req.data);
+            // console.log(req.data);
+            setMovieDetail(req.data);
+            setGenres(req.data.genres);
         } catch (err) {
             console.log('Gagal', err);
         }
@@ -23,7 +26,35 @@ function App() {
 
     const onHandleMovieDetail = (id) => {
         handleMovieDetail(id);
+        setOpen(true);
     };
+
+    const Modal = styled(ModalUnstyled)`
+        position: fixed;
+        z-index: 1300;
+        right: 0;
+        bottom: 0;
+        top: 0;
+        left: 0;
+        display: flex;
+        align-items: center;
+        justify-content: center;
+    `;
+
+    const style = (theme) => ({
+        position: 'absolute',
+        top: '50%',
+        left: '50%',
+        transform: 'translate(-50%, -50%)',
+        width: 400,
+        backgroundColor: theme.palette.mode === 'dark' ? '#0A1929' : 'white',
+        border: '2px solid currentColor',
+        boxShadow: 24,
+        display: 'flex',
+        flexDirection: 'column',
+        justifyContent: 'center',
+        alignItems: 'center',
+    });
 
     const handleSearch = (e) => {
         setKeyword(e.target.value);
@@ -75,6 +106,35 @@ function App() {
                         </div>
                     );
                 })}
+            </div>
+            <div>
+                <Modal
+                    aria-labelledby='transition-modal-title'
+                    aria-describedby='transition-modal-description'
+                    open={open}
+                    onClose={handleClose}
+                    closeAfterTransition
+                    // onClick={handleClose}
+                    sx={{
+                        backgroundColor: 'rgba(0, 0, 0, 0.5)',
+                        WebkitTapHighlightColor: 'transparent',
+                    }}
+                >
+                    <Fade in={open} timeout={400}>
+                        <Box sx={style} key={movieDetail.id}>
+                            <h3 onClick={handleClose}>X</h3>
+                            <h2 id='transition-modal-title'>{movieDetail.title}</h2>
+                            <img id='movie-img' src={`${movieIMG}/${movieDetail.poster_path}`} />
+                            {genres.map((genre, index) => {
+                                return (
+                                    <div key={index}>
+                                        <li>{genre.name}</li>
+                                    </div>
+                                );
+                            })}
+                        </Box>
+                    </Fade>
+                </Modal>
             </div>
         </div>
     );
